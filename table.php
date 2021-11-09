372341b020a7a5cb44612f0f9a0bbbab24137e4c
NTphatta <89444883+NTphatta@users.noreply.github.com>
2021-11-09 09:38:21 +0700

    Create table.php

---
+++ table.php
@@ -0,0 +1,18 @@
+<html>
+  <head></head>
+  <body>
+    <table border="1">
+      <tr>
+        <td>colum 1</td>
+        <td>colum 2</td>
+        <td>colum 3</td>
+      </tr>
+      <tr>
+        <td>colum 1</td>
+        <td>colum 2</td>
+        <td>colum 3</td>
+      </tr>
+    </table>
+  </body>  
+ </head>
+</html>
