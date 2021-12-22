78c6d234a7fbd793bd06c4794bb03b3210f66254
NTphatta <89444883+NTphatta@users.noreply.github.com>
2021-12-22 09:27:39 +0700

    Update province2.php

--- province2.php
+++ province2.php
@@ -1,10 +1,10 @@
 <?php
 
-  echo "<center>รายงานสถานการโควิด-19 แยกจังหวัด</center>";
+  echo "<center><b>รายงานสถานการโควิด-19 แยกจังหวัด</b></center>";
   $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces");
   $data = json_decode($json);
   
-  echo "<table border='8'>";
+  echo "<table border='8'>";"<table width="50%" bgcolor = "pink" >";
   echo "<tr><td><b>ลำดับ</td></b><td><b>จังหวัด</td></b><td><b>ยอดผู้ป่วยใหม่</td></b><td><b>ยอดผู้ป่วยรวม</td></b><td><b>ยอดผู้ป่วยใหม่ภายในประเทศ</td></b><td><b>ยอดผู้ป่วยรวมในประเทศ</td></b>
   <td><b>ยอดผู้เสียชีวิตใหม่</td></b><td><b>ยอดผู้เสียชีวิตรวม</td></b><td><b>วันที่</td></b></tr>";
   foreach($data as $key=>$val){
