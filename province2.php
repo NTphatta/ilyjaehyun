bb7728514756bd3328ac24c836fa4923f5dcc61b
NTphatta <89444883+NTphatta@users.noreply.github.com>
2021-12-22 09:05:26 +0700

    Update province2.php

--- province2.php
+++ province2.php
@@ -1,6 +1,6 @@
 <?php
 
-  echo "<align = 'center'><tr><td>รายงานสถานการโควิด-19 แยกจังหวัด</td></tr></center>";
+  echo "<center><tr><td>รายงานสถานการโควิด-19 แยกจังหวัด</td></tr></center>";
   $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces");
   $data = json_decode($json);
   
