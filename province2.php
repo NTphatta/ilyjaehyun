abc2b06c3d152ca833fd5ba3f8474fb348a8e9a6
NTphatta <89444883+NTphatta@users.noreply.github.com>
2021-12-22 10:18:36 +0700

    Update province2.php

--- province2.php
+++ province2.php
@@ -4,10 +4,10 @@
   $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces");
   $data = json_decode($json);
   
-  $bgcolor[0] = "#FF9966"; $bgcolor[1] = "#CCCCFF"; $bgcolor[2] = "#CCCCFF"; $bgcolor[3] = "#CCCCFF"; $bgcolor[4] = "#CCCCFF"; $bgcolor[5] = "#CCCCFF"; $bgcolor[6] = "#CCCCFF"; 
-$bgcolor[7] = "#CCCCFF"; $bgcolor[8] = "#CCCCFF"; 
+  echo"<$bgcolor[0] = "#FF9966"; $bgcolor[1] = "#CCCCFF"; $bgcolor[2] = "#CCCCFF"; $bgcolor[3] = "#CCCCFF"; $bgcolor[4] = "#CCCCFF"; $bgcolor[5] = "#CCCCFF"; $bgcolor[6] = "#CCCCFF"; 
+$bgcolor[7] = "#CCCCFF"; $bgcolor[8] = "#CCCCFF">"; 
   echo "<table border='8'>";
-  echo "<tr><td><b>ลำดับ</td></b><td><b>จังหวัด</td></b><td><b>ยอดผู้ป่วยใหม่</td></b><td><b>ยอดผู้ป่วยรวม</td></b><td><b>ยอดผู้ป่วยใหม่ภายในประเทศ</td></b><td><b>ยอดผู้ป่วยรวมในประเทศ</td></b>
+  echo "<tr><tdbgcolor="'.$bgcolor[$bt].'"><b>ลำดับ</td></b><td><b>จังหวัด</td></b><td><b>ยอดผู้ป่วยใหม่</td></b><td><b>ยอดผู้ป่วยรวม</td></b><td><b>ยอดผู้ป่วยใหม่ภายในประเทศ</td></b><td><b>ยอดผู้ป่วยรวมในประเทศ</td></b>
   <td><b>ยอดผู้เสียชีวิตใหม่</td></b><td><b>ยอดผู้เสียชีวิตรวม</td></b><td><b>วันที่</td></b></tr>";
   foreach($data as $key=>$val){
     echo "<tr>";
