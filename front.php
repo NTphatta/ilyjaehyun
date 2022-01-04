644d9330edf213c8c2a537e98162b2a3570318fa
NTphatta <89444883+NTphatta@users.noreply.github.com>
2022-01-04 09:27:29 +0700

    Update front.php

--- front.php
+++ front.php
@@ -9,18 +9,30 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     <title>Hello, world!</title>
+    
+    <style>
+      #col1{
+        background-cocol:pink
+      }
+       #col2{
+        background-cocol:red
+      } #col3{
+        background-cocol:orange
+      }
+    </style>  
+    
   </head>
   <body>
     
     <div class="container">
      <div class="row">
-     <div class="col">
+     <div id="col1" class="col-4">
       Column
     </div>
-     <div class="col">
+    <div id="col2" class="col-4">
       Column
     </div>
-     <div class="col">
+    <div id"col3"class="col-4">
       Column
     </div>
    </div>
